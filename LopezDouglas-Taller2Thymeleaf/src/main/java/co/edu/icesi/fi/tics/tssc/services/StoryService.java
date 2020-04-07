1d2b6f9ea8fa4484841a411aab4f99ad6786e131
DouglasPapu <douglas.lopez1@correo.icesi.edu.co>
2020-04-07 01:40:44 -0500

    Actualizando

--- LopezDouglas-Taller2Thymeleaf/src/main/java/co/edu/icesi/fi/tics/tssc/services/StoryService.java
+++
@@ -1,9 +0,0 @@
-package co.edu.icesi.fi.tics.tssc.services;
-
-import co.edu.icesi.fi.tics.tssc.model.TsscStory;
-
-public interface StoryService {
-
-	public TsscStory saveStory(TsscStory nuevo, long id) throws Exception;
-	public TsscStory editStory(TsscStory editado) throws Exception;
-}
